189ca3d106023e526c1c347a4e684139bc020e66
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-03-12 23:16:54 +0900

    [fix] Score.test.tsx/テストケースの重複を削除してリファクタリング

--- src/__tests__/components/Score.test.tsx
+++ src/__tests__/components/Score.test.tsx
@@ -3,40 +3,9 @@ import { render, screen } from "@testing-library/react";
 import { Score } from "src/components/Score";
 import { BoardStatus } from "src/types/BoardStatus";
 
-describe("Score", () => {
-  test("Scoreコンポーネントの初期レンダリング", () => {
-    const initialBoardStatus: BoardStatus = [
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "white", "black", "", "", ""],
-      ["", "", "", "black", "white", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-    ];
-    render(<Score boardStatus={initialBoardStatus} />);
-
-    const blackStoneNumberText = screen.getByText("Black: 2");
-    const whiteStoneNumberText = screen.getByText("White: 2");
-    expect(blackStoneNumberText).toBeInTheDocument();
-    expect(whiteStoneNumberText).toBeInTheDocument();
-  });
-
-  test("boardStatusが変更されたとき、Scoreコンポーネントを再レンダリングする", () => {
-    const initialBoardStatus: BoardStatus = [
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "white", "black", "", "", ""],
-      ["", "", "", "black", "white", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-    ];
-    const { rerender } = render(<Score boardStatus={initialBoardStatus} />);
-
-    const newBoardStatus: BoardStatus = [
+describe("Scoreコンポーネント", () => {
+  test("レンダリング", () => {
+    const boardStatus: BoardStatus = [
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "black", "", "black", "", "", "", ""],
@@ -46,17 +15,11 @@ describe("Score", () => {
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
     ];
+    render(<Score boardStatus={boardStatus} />);
 
-    const blackStoneNumberText = screen.getByText("Black: 2");
-    const whiteStoneNumberText = screen.getByText("White: 2");
+    const blackStoneNumberText = screen.getByText("Black: 3");
+    const whiteStoneNumberText = screen.getByText("White: 6");
     expect(blackStoneNumberText).toBeInTheDocument();
     expect(whiteStoneNumberText).toBeInTheDocument();
-
-    rerender(<Score boardStatus={newBoardStatus} />);
-
-    const newBlackStoneNumberText = screen.getByText("Black: 3");
-    const newWhiteStoneNumberText = screen.getByText("White: 6");
-    expect(newBlackStoneNumberText).toBeInTheDocument();
-    expect(newWhiteStoneNumberText).toBeInTheDocument();
   });
 });

2f936d72268a6eb5ffe71608f28fcaaa8cf19524
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-03-13 10:23:04 +0900

    [rm] CurrentPlayer.test.tsx/テスト不要な箇所のテストを削除

--- src/__tests__/components/CurrentPlayer.test.tsx
+++ src/__tests__/components/CurrentPlayer.test.tsx
@@ -5,16 +5,12 @@ import { CurrentPlayer } from "src/components/CurrentPlayer";
 describe("CurrentPlayer", () => {
   it("currentPlayerがwhiteのときのレンダリング", () => {
     render(<CurrentPlayer currentPlayer="white" />);
-    const text = screen.getByText("Current player is");
     const currentPlayerText = screen.getByText("white");
-    expect(text).toBeInTheDocument();
     expect(currentPlayerText).toBeInTheDocument();
   });
   it("currentPlayerがblackのときのレンダリング", () => {
     render(<CurrentPlayer currentPlayer="black" />);
-    const text = screen.getByText("Current player is");
     const currentPlayerText = screen.getByText("black");
-    expect(text).toBeInTheDocument();
     expect(currentPlayerText).toBeInTheDocument();
   });
 });

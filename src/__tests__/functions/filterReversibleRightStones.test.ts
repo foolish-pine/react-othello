4056f5efa4f3e137cd368827c9adb2bed0796bda
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-03-19 22:40:00 +0900

    [add] filterReversibleRightStones.test.ts/両方のnewStoneColorについてテストできるようテスト項目追加

--- src/__tests__/functions/filterReversibleRightStones.test.ts
+++ src/__tests__/functions/filterReversibleRightStones.test.ts
@@ -8,12 +8,16 @@ describe("filterReversibleRightStones関数", () => {
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "white", "white", "black", "", "", ""],
-      ["", "", "", "black", "white", "", "", ""],
+      ["", "", "black", "black", "white", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
     ];
 
+    expect(filterReversibleRightStones(boardStatus, "white", 4, 1)).toEqual([
+      [4, 2],
+      [4, 3],
+    ]);
     expect(filterReversibleRightStones(boardStatus, "black", 3, 1)).toEqual([
       [3, 2],
       [3, 3],
@@ -24,41 +28,44 @@ describe("filterReversibleRightStones関数", () => {
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
-      ["", "", "white", "black", "", "", "", ""],
+      ["", "", "", "white", "black", "", "", ""],
       ["", "", "", "black", "white", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
     ];
 
-    expect(filterReversibleRightStones(boardStatus, "white", 3, 2)).toEqual([]);
+    expect(filterReversibleRightStones(boardStatus, "white", 3, 3)).toEqual([]);
+    expect(filterReversibleRightStones(boardStatus, "black", 3, 3)).toEqual([]);
   });
   it("選択したマスの右側で、選択したマスと他の石の間に空きマスがあるとき、空の配列を返す", () => {
     const boardStatus: BoardStatus = [
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
-      ["", "", "white", "black", "", "", "", ""],
+      ["", "", "", "white", "black", "", "", ""],
       ["", "", "", "black", "white", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
     ];
 
-    expect(filterReversibleRightStones(boardStatus, "white", 3, 0)).toEqual([]);
+    expect(filterReversibleRightStones(boardStatus, "white", 4, 1)).toEqual([]);
+    expect(filterReversibleRightStones(boardStatus, "black", 3, 1)).toEqual([]);
   });
   it("選択したマスの右側にnewStoneColorの石がないとき、空の配列を返す", () => {
     const boardStatus: BoardStatus = [
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
-      ["", "black", "black", "black", "black", "black", "black", "black"],
-      ["", "", "", "black", "white", "", "", ""],
+      ["", "", "", "black", "black", "black", "black", "black"],
+      ["", "", "", "white", "white", "white", "white", "white"],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
       ["", "", "", "", "", "", "", ""],
     ];
 
-    expect(filterReversibleRightStones(boardStatus, "white", 3, 0)).toEqual([]);
+    expect(filterReversibleRightStones(boardStatus, "white", 4, 2)).toEqual([]);
+    expect(filterReversibleRightStones(boardStatus, "black", 3, 2)).toEqual([]);
   });
 });
